2381274f21a86995199032b8af265ca9f6cd5d60
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-09-04 23:16:26 +0530

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,36 @@
+
+import cv2 as cv
+  
+  
+vid = cv.VideoCapture(0) 
+  
+while(True): 
+
+    ret, frame = vid.read() 
+    
+    gray_frame=cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+    
+    cascade = cv.CascadeClassifier('model\haarcascade_frontalface_default.xml') 
+    
+    faces_rect = cascade.detectMultiScale(gray_frame, 1.1, 9) 
+        
+    
+    for (x, y, w, h) in faces_rect: 
+        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        
+    prediction="With mask"
+
+    
+    if ret:
+        if prediction:
+            font = cv.FONT_HERSHEY_SIMPLEX
+            org = (50, 50)
+            fontScale = 1
+            color = (0, 255, 0)
+            thickness = 2
+            cv.putText(frame, prediction, org=org, fontFace=font, fontScale=fontScale, thickness=thickness, color=color)  
+        cv.imshow('frame', frame)
+
+    if cv.waitKey(1) & 0xFF == ord('q'): 
+        break
+  
