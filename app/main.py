64cdbbea5a72a6d67b2db6aeb3dfb0c64da1d3d0
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-09-05 01:01:46 +0530

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 
+import os
 import cv2 as cv
+from prediction import makePrediction
+
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+os.environ['TF_ENABLE_ONEDNN_OPTS']='0'
   
   
 vid = cv.VideoCapture(0) 
@@ -10,7 +15,7 @@ while(True):
     
     gray_frame=cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
     
-    cascade = cv.CascadeClassifier('model\haarcascade_frontalface_default.xml') 
+    cascade = cv.CascadeClassifier('model/haarcascade_frontalface_default.xml') 
     
     faces_rect = cascade.detectMultiScale(gray_frame, 1.1, 9) 
         
@@ -18,7 +23,7 @@ while(True):
     for (x, y, w, h) in faces_rect: 
         cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         
-    prediction="With mask"
+    prediction, confidence=makePrediction(image=frame)
 
     
     if ret:
